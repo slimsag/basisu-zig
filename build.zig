62a1ebd9c54c79f5d161c90991daeb651e139217
Stephen Gutekanst <stephen@hexops.com>
2023-06-25 09:23:45 -0700

    add module
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -4,6 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("basisu-zig", .{
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "basisu-zig",
         .root_source_file = .{ .path = "src/main.zig" },
